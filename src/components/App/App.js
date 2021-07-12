3bffccf88f893ad0940cd938ee40d6927725da94
Alexander Vitchinov <vitalex92@yandex.ru>
2021-07-12 22:21:34 +0300

    preloader

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import Preloader from '../Preloader/Preloader';
 import SignupSignin from "../Signup-signin/Signup-signin";
 
 function App() {
+  const [isLoading, setIsLoaded] = useState(false);
+
+  useLayoutEffect( () => {
+    setIsLoaded(true);
+  }, [])
+
+  setInterval(() => setIsLoaded(false), 5000);
+
   return (
     <div className="App">
       <div className="page">
-        <SignupSignin/>
+        {isLoading ? <Preloader /> : <SignupSignin/>}
       </div>
     </div>
   );
---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,9 @@
+function Preloader () {
+  return (
+    <section className="preloader">
+      <div className="preloader__round"></div>
+    </section>
+  )
+}
+
+export default Preloader;
